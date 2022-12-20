aa8ef05e32102c3ed29880a047d0833f93f68381
adamiko2015 <adamiko2015@gmail.com>
2022-12-20 17:23:05 +0200

    Update PIDControl.java

--- src/main/java/frc/robot/commands/PIDControl.java
+++ src/main/java/frc/robot/commands/PIDControl.java
@@ -4,38 +4,65 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.Constants;
 import frc.robot.subsystems.Drivetrain;
 
 public class PIDControl extends CommandBase {
 
   private Drivetrain drivetrain;
-  private PIDController pid;
 
   private int setpoint;
+  private double kp, ki, kd;
 
-  public PIDControl(Drivetrain drivetrain, int setpoint) {
+  private double integralSum = 0;
+
+  private long prev_time = System.currentTimeMillis();
+  private int prev_error;
+
+  public PIDControl(Drivetrain drivetrain, int setpoint, double kp, double ki, double kd) {
     this.drivetrain = drivetrain;
     this.setpoint = setpoint;
 
-    pid = new PIDController(Constants.PID.kP, Constants.PID.kI, Constants.PID.kD);
+    this.kp = kp;
+    this.ki = ki;
+    this.kd = kd;
 
     addRequirements(drivetrain);
   }
 
+  public double calculateOutput(int encoder, int setpoint) {
+    int current_error = setpoint - encoder;
+    integralSum += current_error;
+
+    long current_time = System.currentTimeMillis(); 
+
+    double p_out =  current_error * kp;
+    double i_out = integralSum * ki;
+    double d_out = ((double) (current_error - prev_error) / (current_time - prev_time)) * kd;
+
+    prev_time = current_time;
+    prev_error = current_error;
+
+    return p_out + i_out + d_out;
+  }
+
+  public int getError() {
+    return setpoint - drivetrain.getEncoder();
+  }
+  
   @Override
-  public void initialize() {}
+  public void initialize() {
+    prev_error = drivetrain.getEncoder() - setpoint;
+  }
 
   @Override
   public void execute() {
-    double output = pid.calculate(drivetrain.getEncoder(), setpoint);
+    double output = calculateOutput(drivetrain.getEncoder(), setpoint);
     drivetrain.setVictor(output);
 
     SmartDashboard.putNumber("Output", output);
-    SmartDashboard.putNumber("Error", pid.getPositionError());
+    SmartDashboard.putNumber("Error", getError());
   }
 
   @Override
